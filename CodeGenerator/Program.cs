fc83911e2634822690b22fcf745f06bd1cc69ed5
Çağatay UNCU <cagatay.uncu@hotmail.com>
2023-02-10 22:30:01 +0300

    Add project files.

---
+++ CodeGenerator/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("Kaç adet kod üretmek istersiniz?");
+int numberOfCodes = Convert.ToInt32(Console.ReadLine());
+string characters = "ACDEFGHKLMNPRTXYZ234579";
+int length = 8;
+var random = new Random();
+var codes = new List<string>();
+
+while (codes.Count < numberOfCodes)
+{
+    var result = new string(Enumerable.Repeat(characters, length)
+        .Select(s => s[random.Next(s.Length)]).ToArray());
+    if (!codes.Contains(result))
+    {
+        codes.Add(result);
+    }
+}
+
+Console.WriteLine("Üretilen Kodlar: ");
+foreach (var code in codes)
+{
+    Console.WriteLine(code);
+}
